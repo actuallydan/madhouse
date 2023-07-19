0322ebd8b22d514215058258c826942662798b5e
Dan Kral <dankral01@gmail.com>
2023-07-19 13:06:04 -0400

    friggin drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "drizzle-kit";
-
-console.log(process.env.DATABASE_HOST);
+import { env } from "~/env.mjs";
 
 export default {
   schema: "./db/schema.ts",
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: "default",
+  dbCredentials: {
+    host: env.DATABASE_HOST,
+    user: env.DATABASE_USERNAME,
+    password: env.DATABASE_PASSWORD,
+    database: "default",
+  },
 } satisfies Config;
